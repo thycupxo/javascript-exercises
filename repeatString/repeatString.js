f531f4248c2c1fb8dcaec7629971a4085549dad2
Thy Nguyen <thynguyenxo@gmail.com>
2023-03-16 19:34:47 -0700

    updated with my answer

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (word, freq) {
+  let result = "";
+  if (freq < 0) return "ERROR"
 
-};
+  for (let i = 0; i < freq; i++) {
+    result = word.repeat(freq);
+  }
+  return result;
+}
 
 module.exports = repeatString;
