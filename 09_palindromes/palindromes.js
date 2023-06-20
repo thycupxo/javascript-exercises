729730ceb10dd5fcb7e2aa9f58bf38a7724f7059
Thy Nguyen <thynguyenxo@gmail.com>
2023-06-20 13:10:19 -0700

    upload completed exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {};
+const palindromes = function (input) {
+  let inputLowercase = input.toLowerCase();
+  const originalString = inputLowercase.replace(/\W|_/g, '').split("");
+  let inputReverse = inputLowercase.replace(/\W|_/g, '').split("").reverse();
+
+  for (let i = 0; i < originalString.length; i++) {
+    if (originalString[i] !== inputReverse[i]) {
+      return false;
+    }
+  }
+  return true;
 
 };
-
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  //convert to number
+  if (typeof num === "string") {
+    Number(num);
+  } else if (num < 0) {
+    return 'OOPS';
+  }
 
+  const fibonacciArray = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fibonacciArray.push(fibonacciArray[i - 2] + fibonacciArray[i - 1]);
+  }
+  return fibonacciArray[num]
 };
 
 module.exports = fibonacci;
